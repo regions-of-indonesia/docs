c434d244ab99aa02fa76d602914311082a5bb5cb
flamrdevs <flamrdevs@gmail.com>
2023-08-15 14:29:46 +0700

    chore: update workspace

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { AstroUserConfig } from "astro";
 
+import mdx from "@astrojs/mdx";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -7,7 +8,15 @@ import compress from "astro-compress";
 
 export default {
   site: "https://regions-of-indonesia.pages.dev",
-  integrations: [tailwind(), solid(), sitemap(), compress()],
+  integrations: [
+    mdx(),
+    solid(),
+    tailwind(),
+    sitemap(),
+    compress({
+      Logger: 0,
+    }),
+  ],
   server: { host: true, port: 1808 },
   compressHTML: true,
 } satisfies AstroUserConfig;
